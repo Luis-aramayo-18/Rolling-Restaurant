93af2b68fe1bd8b328cbda0475b69b4b6ddaf452
fabi ibañez <aerosfabi@hotmail.com>
2023-02-19 19:32:26 -0300

    implemento lista de productos para administracion

--- src/components/Administracion/ListAdmin/ListAdmin.jsx
+++ src/components/Administracion/ListAdmin/ListAdmin.jsx
@@ -1,13 +1,70 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import ItemAdmin from "../ItemAdmin/ItemAdmin";
+
+const fakeArray = [{
+  id:1,
+  nombre:"producto 1",
+  categoria:"comidas",
+  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200xgoogle.com/jpg",
+  precio: 23.23,
+  descripcion: "blabla",
+  disponible:"si",
+}, {
+  id:2,
+  nombre:"producto 1",
+  categoria:"comidas",
+  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
+  precio: 23.23,
+  descripcion: "blabla",
+  disponible:"si",
+}, {
+  id:3,
+  nombre:"producto 1",
+  categoria:"comidas",
+  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
+  precio: 23.23,
+  descripcion: "blabla",
+  disponible:"si",
+}, {
+  id:4,
+  nombre:"producto 1",
+  categoria:"comidas",
+  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
+  precio: 23.23,
+  descripcion: "blabla",
+  disponible:"si",
+}, {
+  id:5,
+  nombre:"producto 1",
+  categoria:"comidas",
+  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
+  precio: 23.23,
+  descripcion: "blabla",
+  disponible:"si",
+}, {
+  id:6,
+  nombre:"producto 1",
+  categoria:"comidas",
+  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
+  precio: 23.23,
+  descripcion: "blabla",
+  disponible:"si",
+}];
+
 
 
 const ListAdmin = () => {
   return (
-    <div>
-     
-     
-    </div>
-  
+    <Row>
+      {fakeArray.map((elemento) => {
+        return (
+          <Col xs={12} md={4} lg={3} key={elemento.id} className="p-2">
+            <ItemAdmin  {...elemento}/>
+          </Col>
+        );
+      })}
+    </Row>
   );
 };
 
--- src/components/Administracion/MainAdmin/MainAdmin.jsx
+++ src/components/Administracion/MainAdmin/MainAdmin.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import FormAdmin from "../FormAdmin/FormAdmin";
 import ListAdmin from "../ListAdmin/ListAdmin";
 import "./mainAdmin.css";
@@ -7,7 +8,9 @@ const MainAdmin = () => {
   return (
     <div className="mainAdmin">
       <FormAdmin />
-      <ListAdmin />
+      <Container className="my-5">
+        <ListAdmin />
+      </Container>
     </div>
   );
 };
