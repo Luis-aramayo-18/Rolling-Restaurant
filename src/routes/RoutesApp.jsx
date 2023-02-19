d776804194d5bbcc70cd3ab256d50eb01bebf6b3
fabi ibañez <aerosfabi@hotmail.com>
2023-02-19 12:46:21 -0300

    correccion de ruta de administracion y reordenado error 404

--- src/routes/RoutesApp.jsx
+++ src/routes/RoutesApp.jsx
@@ -1,39 +1,46 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import BarraNav from '../components/BarraNav/BarraNav'
-import Footer from '../components/Footer/Footer'
-import FormCliente from '../components/FormCliente/FormCliente'
-import Login from '../components/Login/Login'
-import AboutUs from '../pages/AboutUs'
-import Administracion from '../pages/Administracion'
-import Contact from '../pages/Contact'
-import Home from '../pages/Home'
-import NotFound from '../pages/NotFound'
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BarraNav from "../components/BarraNav/BarraNav";
+import Footer from "../components/Footer/Footer";
+import FormCliente from "../components/FormCliente/FormCliente";
+import Login from "../components/Login/Login";
+import AboutUs from "../pages/AboutUs";
+import Administracion from "../pages/Administracion";
+import Contact from "../pages/Contact";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
 
 const RoutesApp = () => {
   const [clients, setClients] = useState([]);
 
-  const changeClientsList = (newList)=>{
+  const changeClientsList = (newList) => {
     setClients(newList);
 
     localStorage.setItem("clientes", JSON.stringify(newList));
-
-  }
+  };
   return (
     <BrowserRouter>
-      <BarraNav/>
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/nosotros' element={<AboutUs/>}/>
-            <Route path='/contacto' element={<Contact/>}/>
-            <Route path='/*' element={<NotFound/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/registro' element={<FormCliente changeClientsList={changeClientsList} clientes={clients}/>}/>
-            <Route path="/Administracion" element={<Administracion/>}/>
-        </Routes>
-      <Footer/>
+      <BarraNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nosotros" element={<AboutUs />} />
+        <Route path="/contacto" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/registro"
+          element={
+            <FormCliente
+              changeClientsList={changeClientsList}
+              clientes={clients}
+            />
+          }
+        />
+        <Route path="/administracion" element={<Administracion />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default RoutesApp
+export default RoutesApp;
