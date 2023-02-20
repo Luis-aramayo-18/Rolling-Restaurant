1e2a24d6dec6d5b09cc875ff1bb43061d2ea09af
fabi ibañez <aerosfabi@hotmail.com>
2023-02-19 22:55:05 -0300

    agrego variable de entorno para conectarse a la base de datos (me habia olvidado)

--- src/components/Administracion/ListAdmin/ListAdmin.jsx
+++ src/components/Administracion/ListAdmin/ListAdmin.jsx
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import ItemAdmin from "../ItemAdmin/ItemAdmin";
 
+const baseUrl = process.env.REACT_APP_BASE_URL;
+
 const ListAdmin = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     const fetchItems = async () => {
-      const data = await axios.get("http://localhost:3004/items");
+      const data = await axios.get(`${baseUrl}/items`);
       setItems(data.data);
     };
     fetchItems();
