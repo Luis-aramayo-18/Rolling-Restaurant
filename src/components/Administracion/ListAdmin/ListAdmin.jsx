d89706198f19c433dd693191c9507287f46eafbe
fabi ibañez <aerosfabi@hotmail.com>
2023-02-19 22:44:37 -0300

    implementé axios y cree archivos de entorno .env y ejemplo .env.sample

--- src/components/Administracion/ListAdmin/ListAdmin.jsx
+++ src/components/Administracion/ListAdmin/ListAdmin.jsx
@@ -1,66 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import ItemAdmin from "../ItemAdmin/ItemAdmin";
 
-const fakeArray = [{
-  id:1,
-  nombre:"producto 1",
-  categoria:"comidas",
-  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200xgoogle.com/jpg",
-  precio: 23.23,
-  descripcion: "blabla",
-  disponible:"si",
-}, {
-  id:2,
-  nombre:"producto 1",
-  categoria:"comidas",
-  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
-  precio: 23.23,
-  descripcion: "blabla",
-  disponible:"si",
-}, {
-  id:3,
-  nombre:"producto 1",
-  categoria:"comidas",
-  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
-  precio: 23.23,
-  descripcion: "blabla",
-  disponible:"si",
-}, {
-  id:4,
-  nombre:"producto 1",
-  categoria:"comidas",
-  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
-  precio: 23.23,
-  descripcion: "blabla",
-  disponible:"si",
-}, {
-  id:5,
-  nombre:"producto 1",
-  categoria:"comidas",
-  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
-  precio: 23.23,
-  descripcion: "blabla",
-  disponible:"si",
-}, {
-  id:6,
-  nombre:"producto 1",
-  categoria:"comidas",
-  url_imagen: "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2020/08/hamburguesa-2028707.jpg?tf=1200x",
-  precio: 23.23,
-  descripcion: "blabla",
-  disponible:"si",
-}];
-
-
-
 const ListAdmin = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const data = await axios.get("http://localhost:3004/items");
+      setItems(data.data);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <Row>
-      {fakeArray.map((elemento) => {
+      {items.map((elemento) => {
         return (
           <Col xs={12} md={4} lg={3} key={elemento.id} className="p-2">
-            <ItemAdmin  {...elemento}/>
+            <ItemAdmin {...elemento} />
           </Col>
         );
       })}
