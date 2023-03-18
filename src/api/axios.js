3a093431133e4250f73eed9ceb7b53a52d4c1b45
Luis Aramayo <Aramayo.Luis.18@gmail.com>
2023-03-17 23:48:53 -0300

    bla

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,12 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+const baseURL = process.env.REACT_APP_BACKEND_BASE_URL
 
-// TODO: Revisar por qué sigue trayendo el token anterior
-const myAxios = () => {
-  const token = sessionStorage.getItem('token');
-
-  return axios.create({
-    baseURL: baseUrl,
+const myAxios = axios.create({
+    baseURL: baseURL,
     headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-};
+        Authorization: `Bearer ${sessionStorage.getItem("token")}`
+    }
+})
 
-export default myAxios;
+export default myAxios
