b4f3c1c6a4f44f689075785612124ab59edb220c
Paula Bejarano <111022034+paubejarano@users.noreply.github.com>
2023-04-05 15:33:41 -0300

    Corrección de responsividad del panel de admin

--- src/components/Administracion/MainAdmin/MainAdmin.jsx
+++ src/components/Administracion/MainAdmin/MainAdmin.jsx
@@ -2,12 +2,11 @@ import { useState } from "react";
 import { Container } from "react-bootstrap";
 import FormAdmin from "../FormAdmin/FormAdmin";
 import TableAdmin from "../TableAdmin/TableAdmin";
-import "./mainAdmin.css";
 
 const MainAdmin = () => {
   const [modifyingProduct, setModifyingProduct] = useState(null);
   return (
-    <Container className="mainAdmin">
+    <Container>
       <h1 className="text-center">Panel de Administración</h1>
       <hr />
       <FormAdmin modifyingProduct={modifyingProduct} setModifyingProduct={setModifyingProduct} />
